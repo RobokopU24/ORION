4fb153a29413d2ea4e3e5d644a2d380c8300afa5
Phil Owen <phillipsowen@outlook.com>
2020-07-02 15:35:06 -0400

    refactoring, moving common functionality in.

--- Common/utils.py
+++ Common/utils.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import logging
+import tarfile
+import csv
+from csv import reader
 from ftplib import FTP
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
@@ -86,7 +89,9 @@ class LoggingUtil(object):
 
 
 class NodeNormUtils:
-    @staticmethod
+    # create a logger
+    logger = LoggingUtil.init_logging("Data_services.Common.NodeNormUtils", line_format='medium', log_file_path=os.path.join(Path(__file__).parents[1], 'logs'))
+
     def normalize_node_data(self, cached_node_norms, node_list: list) -> list:
         """
         This method calls the NodeNormalization web service to get the normalized identifier and name of the taxon node.
@@ -96,9 +101,6 @@ class NodeNormUtils:
         :param node_list: A list with items to normalize
         :return:
         """
-
-        # create a logger
-        logger = LoggingUtil.init_logging("Data_services.Common.NodeNormUtils", line_format='medium', log_file_path=os.path.join(Path(__file__).parents[1], 'logs'))
 
         # loop through the list and only save the NCBI taxa nodes
         node_idx: int = 0
@@ -117,7 +119,7 @@ class NodeNormUtils:
                 if not node_list[node_idx]['id'] in cached_node_norms:
                     tmp_normalize.add(node_list[node_idx]['id'])
                 else:
-                    logger.debug(f"Cache hit: {node_list[node_idx]['id']}")
+                    self.logger.debug(f"Cache hit: {node_list[node_idx]['id']}")
 
             node_idx += 1
 
@@ -133,7 +135,7 @@ class NodeNormUtils:
         # get the last index of the list
         last_index: int = len(to_normalize)
 
-        logger.info(f'{last_index} unique nodes will be normalized.')
+        self.logger.debug(f'{last_index} unique nodes will be normalized.')
 
         # grab chunks of the data frame
         while True:
@@ -145,7 +147,7 @@ class NodeNormUtils:
                 if end_index >= last_index:
                     end_index = last_index
 
-                self.print_debug_msg(f'Working block {start_index} to {end_index}.')
+                self.logger.debug(f'Working block {start_index} to {end_index}.')
 
                 # collect a slice of records from the data frame
                 data_chunk: list = to_normalize[start_index: end_index]
@@ -165,7 +167,7 @@ class NodeNormUtils:
                     cached_node_norms = merged
                 else:
                     # the 404 error that is trapped here means that the entire list of nodes didnt get normalized.
-                    logger.debug(f'Response code: {resp.status_code}')
+                    self.logger.debug(f'Response code: {resp.status_code}')
 
                     # since they all failed to normalize add to the list so we dont try them again
                     for item in data_chunk:
@@ -203,7 +205,7 @@ class NodeNormUtils:
                     # find the id and replace it with the normalized value
                     node_list[node_idx]['id'] = cached_node_norms[rv['id']]['id']['identifier']
                 else:
-                    logger.error(f"{rv['id']} has no normalized value")
+                    self.logger.error(f"{rv['id']} has no normalized value")
 
             # go to the next index
             node_idx += 1
@@ -213,25 +215,22 @@ class NodeNormUtils:
 
 
 class GetData:
-    @staticmethod
-    def pull_via_ftp(ftp_site: str, ftp_dir: str, data_file_path: str, ftp_data_file: str) -> bool:
+    # create a logger
+    logger = LoggingUtil.init_logging("Data_services.Common.GetData", line_format='medium', log_file_path=os.path.join(Path(__file__).parents[1], 'logs'))
+
+    def pull_via_ftp(self, ftp_site: str, ftp_dir: str, ftp_files: list, data_file_path: str) -> int:
         """
         gets the requested files from UniProtKB ftp directory
 
         :param ftp_site: url of the ftp site
         :param ftp_dir: the directory in the site
+        :param ftp_files: the name of the file to capture
         :param data_file_path: the destination of the captured file
-        :param ftp_data_file: the name of the file to capture
         :return: boolean pass/fail
         """
 
-        # create a logger
-        logger = LoggingUtil.init_logging("Data_services.Common.GetData", line_format='medium', log_file_path=os.path.join(Path(__file__).parents[1], 'logs'))
-
-        logger.info(f'Retrieving {ftp_data_file} from {ftp_site}{ftp_dir} and placing it in {data_file_path}')
-
-        # init the return value
-        ret_val: bool = False
+        # init a retrieved file counter for the return
+        file_counter: int = 0
 
         try:
             # open the FTP connection and go to the directory
@@ -239,28 +238,172 @@ class GetData:
             ftp.login()
             ftp.cwd(ftp_dir)
 
-            # does the file exist and has data in it
-            try:
-                size: int = os.path.getsize(os.path.join(data_file_path, ftp_data_file))
-            except FileNotFoundError:
-                size: int = 0
+            # if the target directory doesnt exist, create it
+            if not os.path.exists(data_file_path):
+                os.makedirs(data_file_path)
 
-            # if we have a size we done need to get the file
-            if size == 0:
-                # open the file
-                with open(os.path.join(data_file_path, ftp_data_file), 'wb') as fp:
-                    # get the file data into a file
-                    ftp.retrbinary(f'RETR {ftp_data_file}', fp.write)
+            # for each file requested
+            for f in ftp_files:
+                self.logger.debug(f'Retrieving {f} from {ftp_site}{ftp_dir} and placing it in {data_file_path}')
+
+                # does the file exist and has data in it
+                try:
+                    size: int = os.path.getsize(os.path.join(data_file_path, f))
+                except FileNotFoundError:
+                    size: int = 0
+
+                # if we have a size we done need to get the file
+                if size == 0:
+                    # open the file
+                    with open(os.path.join(data_file_path, f), 'wb') as fp:
+                        # get the file data into a file
+                        ftp.retrbinary(f'RETR {f}', fp.write)
+
+                # inform user of progress
+                file_counter += 1
+
+                if file_counter % 50 == 0:
+                    self.logger.debug(f'{file_counter} files retrieved, {len(ftp_files) - file_counter} to go.')
+
+            self.logger.debug(f'{file_counter} files retrieved of {len(ftp_files)} requested.')
 
             # close the ftp object
             ftp.quit()
-
-            # set the return value
-            ret_val = True
-
-            logger.info(f'Retrieval of {ftp_data_file} complete.')
         except Exception as e:
-            logger.error(f'Pull_via_ftp() failed. Exception: {e}')
+            self.logger.error(f'pull_via_ftp() failed. Exception: {e}')
 
         # return pass/fail to the caller
+        return file_counter
+
+    def get_ncbi_taxon_id_set(self, taxon_data_dir, organism_type: str) -> set:
+        """
+        gets the files associated with viruses (and/or maybe bacteria)
+        the nodes.dmp file can be found in the archive: ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+
+        :param: the organism type
+        :return: a list of file indexes
+        """
+        # init the return value
+        ret_val: set = set()
+
+        # the name of the tar file that has the target data file
+        data_file_name = 'taxdump.tar.gz'
+
+        # get the tar file that has the taxon id data
+        self.pull_via_ftp('ftp.ncbi.nih.gov', '/pub/taxonomy', [data_file_name], taxon_data_dir)
+
+        # open the tar file
+        tar_file = tarfile.open(os.path.join(taxon_data_dir, data_file_name), 'r:gz')
+
+        # get a reference to the file
+        fp = tar_file.extractfile('nodes.dmp')
+
+        # read in the file lines
+        lines = fp.readlines()
+
+        # for each line in the file
+        for line in lines:
+            # remove the trailing \t and split by the line delimiter
+            new_line = line.decode('utf-8').strip().split('\t|\t')
+
+            # get all the elements in the line without \t
+            parts = [x.strip() for x in new_line]
+
+            # is this one we are looking for
+            if parts[4] == organism_type:
+                # save it
+                ret_val.add(parts[0])
+
+        # close the files
+        fp.close()
+        tar_file.close()
+
+        # remove the target file
+        os.remove(os.path.join(taxon_data_dir, data_file_name))
+
+        # return the list
         return ret_val
+
+    def get_uniprot_virus_file_list(self, proteome_data_dir: str, organism_type: str, taxa_id_set: set) -> list:
+        """
+        gets the list of virus proteome file names that will be downloaded
+        the proteome2taxid file can be found in the ftp directory at: ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/proteomes/
+
+        :param proteome_data_dir: the location of the proteome to taxon id conversion file
+        :param organism_type: one of two types supported
+        :param taxa_id_set: the set of taxa ids
+        :return: the set of file names to get
+        """
+        # storage for the final file list
+        files: list = []
+
+        # set the data file name
+        data_file_name = 'proteome2taxid'
+
+        # get the proteome to taxon id file
+        self.pull_via_ftp('ftp.ebi.ac.uk', '/pub/databases/GO/goa/proteomes', [data_file_name], proteome_data_dir)
+
+        # open the file
+        with open(os.path.join(proteome_data_dir, data_file_name), 'r') as fp:
+            # create a csv reader for it
+            csv_reader: reader = csv.reader(fp, delimiter='\t')
+
+            # spin through the list and get the file name
+            for line in csv_reader:
+                # is this file in the list of target taxa
+                if line[1] in taxa_id_set:
+                    # save the file in the list
+                    files.append(line[2])
+
+        # sort the file list
+        ret_val: list = sorted(files)
+
+        # get the file name based on organism type
+        if organism_type == organism_type:
+            # add the sars cov-2 file manually
+            ret_val.append('uniprot_sars-cov-2.gaf')
+
+        self.logger.debug(f'{len(ret_val)} total files found.')
+
+        # close the file
+        fp.close()
+
+        # remove the data file
+        os.remove(os.path.join(proteome_data_dir, data_file_name))
+
+        # return the list to the caller
+        return ret_val
+
+    def get_goa_virus_files(self, data_dir: str, file_list: list) -> int:
+        """
+        gets all the uniprot GOA files.
+
+        :param data_dir: the data file(s) destination
+        :param file_list: the list of files
+        :return: the retrieved file count
+        """
+
+        # init some counters
+        attempts: int = 0
+        target_count: int = len(file_list)
+        actual_count: int = 0
+
+        # a connection to this FTP site is not reliable
+        while attempts < 25:
+            try:
+                # get the 1 sars-cov-2 file
+                self.pull_via_ftp('ftp.ebi.ac.uk', '/pub/contrib/goa/', ['uniprot_sars-cov-2.gaf'], data_dir)
+
+                # get the rest of the files
+                actual_count = self.pull_via_ftp('ftp.ebi.ac.uk', '/pub/databases/GO/goa/proteomes/', file_list, data_dir)
+
+                # if we got all the files
+                if target_count != 0 and (target_count == actual_count):
+                    break
+            # handle issues in file retrieval
+            except Exception as e:
+                self.logger.error(f'target: {target_count}, actual: {actual_count}, attempts: {attempts}, {e}')
+                attempts += 1
+
+        # return the number of files captured
+        return actual_count
