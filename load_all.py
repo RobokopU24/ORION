eb4d3b24ab01315c237a41e64bfdb23a07fa1724
Phil Owen <phillipsowen@outlook.com>
2020-07-08 12:16:53 -0400

    adding intact and goa to all loading

--- load_all.py
+++ load_all.py
@@ -2,7 +2,9 @@ import os
 import argparse
 from ViralProteome.src.loadVP import VPLoader
 from ViralProteome.src.loadUniRef import UniRefSimLoader
-from Common.utils import LoggingUtil, GetData
+from IntAct.src.loadIA import IALoader
+from GOA.src.loadGOA import GOALoader
+from Common.utils import LoggingUtil
 from pathlib import Path
 
 # create a logger
@@ -15,54 +17,52 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Load UniProtKB viral proteome and UniRef data files and create KGX import files.')
 
     # command line should be like: python load_all.py -p /projects/stars/Data_services/UniProtKB_data -u /projects/stars/Data_services/UniRef_data -r uniref50,uniref90,uniref100
-    ap.add_argument('-p', '--uniprot_dir', required=True, help='The directory of the UniProtKB data files')
-    ap.add_argument('-u', '--uniref_dir', required=True, help='The directory of the UniRef data files')
-    ap.add_argument('-f', '--uniref_files', required=True, help='Comma separated UniRef data file(s) to parse')
+    ap.add_argument('-p', '--uniprot_dir', required=False, help='The directory of the UniProtKB data files.')
+    ap.add_argument('-r', '--uniref_dir', required=False, help='The directory of the UniRef data files.')
+    ap.add_argument('-i', '--intact_dir', required=False, help='The directory of the IntAct data files.')
+    ap.add_argument('-g', '--goa_dir', required=False, help='The directory of the GOA data files.')
+    ap.add_argument('-f', '--uniref_files', required=False, help='Comma separated UniRef data file(s) to parse.')
 
     # parse the arguments
     args = vars(ap.parse_args())
 
-    # assign the uniprot directory
+    # assign the uniprot dir
     UniProtKB_data_dir = args['uniprot_dir']
 
-    # assign the uniref directory
-    UniRef_data_dir = args['uniref_dir']
+    if UniProtKB_data_dir is not None:
+        # get a reference to the processor
+        vp = VPLoader()
 
-    # assign the uniref files
-    UniRef_files = args['uniref_files'].split(',')
-
-    # get a reference to the processor
-    vp = VPLoader()
-
-    # and get a reference to the data gatherer
-    gd = GetData()
-
-    # get the list of target taxa
-    target_taxa_set: set = gd.get_ncbi_taxon_id_set(UniProtKB_data_dir, vp.TYPE_VIRUS)
-
-    # get the list of files that contain those taxa
-    file_list: list = gd.get_uniprot_virus_file_list(UniProtKB_data_dir, vp.TYPE_VIRUS, target_taxa_set)
-
-    # manually add in the sars cov2 data file
-    file_list.append('uniprot_sars-cov-2.gaf')
-
-    # assign the data directory
-    goa_data_dir = UniProtKB_data_dir + '/Virus_GOA_files/'
-
-    # get the data files
-    actual_count: int = gd.get_goa_files(goa_data_dir, file_list, '/pub/databases/GO/goa', '/proteomes/')
-
-    # did we get all the files
-    if len(file_list) == actual_count:
         # load the data files and create KGX output
-        vp.load(UniProtKB_data_dir, 'Virus_GOA_files/', file_list, 'Viral_proteome_GOA')
-    else:
-        logger.error('Error: Did not receive all the UniProtKB GOA files.')
+        vp.load(UniProtKB_data_dir, 'Viral_proteome_GOA')
 
-    # get a reference to the processor
-    vp = UniRefSimLoader()
+    # assign the uniref directory and target files
+    UniRef_data_dir = args['uniref_dir']
+    UniRef_files = args['uniref_files']
 
-    # load the data files and create KGX output
-    vp.load(UniRef_data_dir, UniRef_files, 'taxon_file_indexes.txt', block_size=10000, debug_files=False)
+    if UniRef_data_dir is not None and UniRef_files is not None:
+        # get a reference to the processor
+        uni = UniRefSimLoader()
 
-    logger.info(f'UniRef data parsing and KGX file creation complete.')
+        # load the data files and create KGX output
+        uni.load(UniRef_data_dir, UniRef_files.split(','), 'taxon_file_indexes.txt')
+
+    # assign the uniref directory
+    IntAct_data_dir = args['intact_dir']
+
+    if IntAct_data_dir is not None:
+        # get a reference to the processor
+        ia = IALoader()
+
+        # load the data files and create KGX output files
+        ia.load(IntAct_data_dir, 'intact')
+
+    # assign the uniref directory
+    GOA_data_dir = args['goa_dir']
+
+    if GOA_data_dir is not None:
+        # get a reference to the processor
+        goa = GOALoader()
+
+        # load the data files and create KGX output files
+        goa.load(GOA_data_dir, '/HUMAN/', 'goa_human.gaf.gz', 'Human_GOA')
