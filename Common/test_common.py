cb2f12818409c8aa4f6c798ae0e057c0ca211419
Phil Owen <phillipsowen@outlook.com>
2020-07-20 16:17:05 -0400

    adding tester for getting a file via http

--- Common/test_common.py
+++ Common/test_common.py
@@ -7,7 +7,7 @@ def test_get_taxon_id_list():
 
     gd = GetData()
 
-    data_file_path = os.path.dirname(os.path.abspath(__file__))
+    data_file_path: str = os.path.dirname(os.path.abspath(__file__))
 
     date_stamp: str = gd.get_uniprot_virus_date_stamp(data_file_path)
 
@@ -27,9 +27,24 @@ def test_get_taxon_id_list():
 
     file_subset: list = file_list[:2]
 
-    actual_count: int = gd.get_goa_files(data_file_path, file_subset, '/pub/databases/GO/goa', '/proteomes/')
+    actual_count: int = gd.get_goa_ftp_files(data_file_path, file_subset, '/pub/databases/GO/goa', '/proteomes/')
 
     assert(actual_count == len(file_subset))
 
     # remove the test data
     shutil.rmtree(data_file_path)
+
+def test_pull_via_http():
+    from Common.utils import GetData
+
+    gd = GetData()
+
+    data_file_path: str = os.path.dirname(os.path.abspath(__file__))
+
+    byte_count: int = gd.pull_via_http('https://renci.org/mission-and-vision', data_file_path)
+
+    assert byte_count
+
+    assert(os.path.exists('mission-and-vision'))
+
+    os.remove('mission-and-vision')
