104d38619b92832651da4593478ea3ffdc39b47c
Phil Owen <phillipsowen@outlook.com>
2020-10-13 08:10:49 -0400

    removing node list de-duplication because it is now done eleswhere in node norm

--- IntAct/src/loadIA.py
+++ IntAct/src/loadIA.py
@@ -348,6 +348,9 @@ class IALoader:
         # node list index counter
         node_idx: int = 0
 
+        # de-dupe the list
+        node_list = [dict(t) for t in {tuple(d.items()) for d in node_list}]
+
         # save the node list count to avoid grabbing it over and over
         node_count: int = len(node_list)
 
