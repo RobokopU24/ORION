b6a1af484ca933e440fff9d3a62ae9f8ebe6e05c
Phil Owen <phillipsowen@outlook.com>
2021-04-05 13:42:44 -0400

    adding call to parent class init

--- PHAROS/src/loadPHAROS.py
+++ PHAROS/src/loadPHAROS.py
@@ -100,6 +100,9 @@ class PHAROSLoader(SourceDataLoader):
         constructor
         :param test_mode - sets the run into test mode
         """
+        # call the super
+        super(SourceDataLoader, self).__init__()
+
         # set global variables
         self.data_path = os.environ['DATA_SERVICES_STORAGE']
         self.test_mode = test_mode
