fbe08bc6d7f4f9e4a6d5e32d014dec8704cfd196
Evan Morris <evandietzmorris@gmail.com>
2021-04-16 22:37:37 -0700

    the storage dir should really be outside of the repo

--- set_up_test_env.sh
+++ set_up_test_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p $PWD/logs
-export DATA_SERVICES_LOGS=$PWD/logs
-export DATA_SERVICES_STORAGE=$PWD/tests/storage
+mkdir -p $PWD/../Data_services_storage/
+mkdir -p $PWD/../Data_services_storage/logs
+export DATA_SERVICES_LOGS=$PWD/../Data_services_storage/logs
+export DATA_SERVICES_STORAGE=$PWD/../Data_services_storage/
 export PYTHONPATH=$PWD
