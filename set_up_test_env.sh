bfedc68de42f2ca5761c6e924bfce4f2c25052df
Evan Morris <evandietzmorris@gmail.com>
2021-06-24 05:09:33 -0400

    copying test data files to the storage dir in the test set up, moved GTEx test data into it's own dir

--- set_up_test_env.sh
+++ set_up_test_env.sh
@@ -4,6 +4,8 @@ export PYTHONPATH=$PWD
 mkdir -p $PWD/../Data_services_storage
 export DATA_SERVICES_STORAGE=$PWD/../Data_services_storage/
 
+cp -r $PWD/tests/resources/* $PWD/../Data_services_storage/
+
 cp $PWD/default-config.yml $PWD/../Data_services_storage/custom-config.yml
 export DATA_SERVICES_CONFIG=custom-config.yml
 
@@ -15,13 +17,3 @@ export DATA_SERVICES_GRAPHS=$PWD/../Data_services_graphs/
 
 cp $PWD/default-graph-spec.yml $PWD/../Data_services_graphs/custom-graph-spec.yml
 export DATA_SERVICES_GRAPH_SPEC=custom-graph-spec.yml
-
-export DS_GRAPH_HOST=ds_graph
-export DS_GRAPH_USER=neo4j
-export DS_GRAPH_PASSWORD=fake_neo4j_password
-export DS_GRAPH_HEAP_MEMORY=4G
-export DS_GRAPH_CACHE_MEMORY=4G
-
-export DS_GRAPH_HTTP_PORT=7474
-export DS_GRAPH_HTTPS_PORT=7473
-export DS_GRAPH_BOLT_PORT=7687
