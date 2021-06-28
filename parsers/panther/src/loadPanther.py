cddeb18da4fe3d2698a58f895395f2d10b8378c2
Phil Owen <phillipsowen@outlook.com>
2021-06-28 10:31:40 -0400

    additions to parser

--- parsers/panther/src/loadPanther.py
+++ parsers/panther/src/loadPanther.py
@@ -10,14 +10,23 @@ from bs4 import BeautifulSoup
 from Common.utils import LoggingUtil, GetData
 from Common.kgx_file_writer import KGXFileWriter
 from Common.loader_interface import SourceDataLoader
+from functools import partial
+from typing import NamedTuple
 
+class LabeledID(NamedTuple):
+    """
+    Labeled Thing Object
+    ---
+    """
+    identifier: str
+    label: str = ''
 
 ##############
-# Class: Panther loader
+# Class: PANTHER loader, Protein ANalysis THrough Evolutionary Relationships
 #
 # By: Phil Owen
 # Date: 4/5/2021
-# Desc: Class that loads/parses the Panther data.
+# Desc: Class that loads/parses the PANTHER data.
 ##############
 class PLoader(SourceDataLoader):
     # the final output lists of nodes and edges
@@ -34,11 +43,26 @@ class PLoader(SourceDataLoader):
 
         # set global variables
         self.data_path: str = os.environ['DATA_SERVICES_STORAGE']
-        self.data_version: str = ''
         self.data_file: str = 'PTHR~_human'
+        self.data_version: str = ''
         self.test_mode: bool = test_mode
         self.source_id: str = 'PANTHER'
         self.source_db: str = 'Protein ANalysis THrough Evolutionary Relationships'
+
+        # the list of columns in the data
+        self.sequence_file_columns = ['gene_identifier', 'protein_id', 'gene_name', 'panther_sf_id', 'panther_family_name',
+                                      'panther_subfamily_name', 'panther_molecular_func', 'panther_biological_process',
+                                      'cellular_components', 'protein_class', 'pathway']
+
+        self.split_mapping = {
+            'gene_identifier': partial(self.split_with, splitter='|', keys=['organism', 'gene_id', 'protein_id']),
+            'panther_molecular_func': partial(self.split_with, splitter=';'),
+            'panther_biological_process': partial(self.split_with, splitter=';'),
+            'cellular_components': partial(self.split_with, splitter=';'),
+            'pathway': partial(self.split_with, splitter=';')
+        }
+
+        self.__gene_family_data__ = None
 
         # create a logger
         self.logger = LoggingUtil.init_logging("Data_services.Panther.PLoader", level=logging.INFO, line_format='medium', log_file_path=os.environ['DATA_SERVICES_LOGS'])
@@ -134,7 +158,6 @@ class PLoader(SourceDataLoader):
         :param edges_output_file_path: the path to the edge file
         :return the parsed metadata stats
         """
-
         self.logger.info(f'Panther - Start of Panther data processing.')
 
         # get the list of taxons to process
@@ -148,7 +171,7 @@ class PLoader(SourceDataLoader):
             self.logger.debug(f'Panther - {self.data_file} archive retrieved. Parsing data.')
 
             # parse the data
-            load_metadata = self.parse_data_file(self.data_path, self.data_file)
+            load_metadata = self.parse_data_file()
 
             self.logger.info(f'Panther - {self.data_file} Processing complete.')
 
@@ -165,44 +188,105 @@ class PLoader(SourceDataLoader):
         # return the metadata to the caller
         return load_metadata
 
-    def parse_data_file(self, data_file_path: str, data_file_name: str) -> dict:
+    def split_with(self, input_str, splitter, keys=[], ignore_length_mismatch=False):
+        """
+        Splits a string based on splitter. If keys is provided it will return a dictionary where the keys of the dictionary map to
+        the splitted values.
+        """
+        split = input_str.split(splitter)
+
+        if not keys:
+            return split
+
+        if not ignore_length_mismatch and len(split) != len(keys):
+            raise Exception("Length of keys provided doesn't match split result")
+
+        return {keys[index]: value for index, value in enumerate(split[:len(keys)])}
+
+    @property
+    def gene_family_data(self):
+        """
+        Property that restructures raw csv values into dictionary organized by family and subfamilies of genes.
+        """
+        rows = []
+
+        # if we have already retrieved the data return it
+        if self.__gene_family_data__:
+            return self.__gene_family_data__
+
+        # open up the file
+        with open(os.path.join(self.data_path, self.data_file), 'r', encoding="utf-8") as fp:
+            # get a handle on the input data
+            data = csv.DictReader(fp, delimiter='\t', fieldnames=self.sequence_file_columns)
+
+            for item in data:
+                rows.append(item)
+
+        with_columns = [{self.sequence_file_columns[index]: value for index, value in enumerate(row)} for row in rows]
+
+        # second pass transform into sub dictionaries for relevant ones
+        for row in with_columns:
+            for key in self.split_mapping:
+                functor = self.split_mapping[key]
+                row[key] = functor(row[key])
+
+        # reorganize them to 'family-key'-'sub-family'
+        self.__gene_family_data__ = {}
+
+        for row in with_columns:
+            fam_id, sub_id = row['panther_sf_id'].split(':')
+            family_name = row['panther_family_name']
+            sub_family_name = row['panther_subfamily_name']
+
+            if fam_id not in self.__gene_family_data__:
+                self.__gene_family_data__[fam_id] = {
+                    'family_name': family_name
+                }
+
+            if sub_id not in self.__gene_family_data__[fam_id]:
+                self.__gene_family_data__[fam_id][sub_id] = {
+                    'sub_family_name': sub_family_name,
+                    'rows': []
+                }
+
+            self.__gene_family_data__[fam_id][sub_id]['rows'].append(row)
+
+        return self.__gene_family_data__
+
+    def parse_data_file(self) -> dict:
         """
         Parses the data file for graph nodes/edges and writes them to the KGX csv files.
 
-        :param data_file_path: the path to the PANTHER file
-        :param data_file_name: the name of the PANTHER file
         :return: ret_val: record counts
         """
-        # get the path to the data file
-        infile_path: str = os.path.join(data_file_path, data_file_name)
-
         # init the record counters
         record_counter: int = 0
         skipped_record_counter: int = 0
 
-        # open up the file
-        with open(infile_path, 'r', encoding="utf-8") as fp:
-            # the list of columns in the data
-            cols = ['gene_identifier', 'protein_id', 'gene_name', 'panther_sf_id', 'panther_family_name',
-                    'panther_subfamily_name', 'panther_molecular_func', 'panther_biological_process',
-                    'cellular_components', 'protein_class', 'pathway']
+        gene_fam_data = self.gene_family_data
 
-            # get a handle on the input data
-            data = csv.DictReader(fp, delimiter='\t', fieldnames=cols)
+        lids: list = []
 
-            # go through the data. no column header in this data
-            for r in data:
-                # increment the counter
-                record_counter += 1
+        for key in gene_fam_data:
+            name = gene_fam_data[key]['family_name']
+            name = f'{name} ({key})' if 'NOT NAMED' in name else name
+            lids.append(LabeledID(f'PANTHER.FAMILY:{key}', name))
+            sub_keys = [k for k in gene_fam_data[key].keys() if k !='family_name']
+            for k in sub_keys:
+                name = gene_fam_data[key][k]['sub_family_name']
+                name = f'{name} ({key})' if 'NOT NAMED' in name else name
+                lids.append(LabeledID(f'PANTHER.FAMILY:{key}:{k}',gene_fam_data[key][k]['sub_family_name'] ))
 
-            fam_id, sub_id = r['panther_sf_id'].split(':')
-            fam_name = r['panther_family_name']
+        #for id in lids:
 
-            self.get_gene_family_by_gene_family(fam_name, fam_id, sub_id, r)
-            self.get_gene_by_gene_family(fam_name, fam_id, r)
-            self.get_cellular_component_by_gene_family(fam_name, fam_id, r)
-            self.get_biological_process_or_activity_by_gene_family(fam_name, fam_id, r)
-            self.get_pathway_by_gene_family(fam_name, fam_id, r)
+        # fam_id, sub_id = r['panther_sf_id'].split(':')
+        # fam_name = r['panther_family_name']
+        #
+        # self.get_gene_family_by_gene_family(fam_name, fam_id, sub_id, r)
+        # self.get_gene_by_gene_family(fam_name, fam_id, r)
+        # self.get_cellular_component_by_gene_family(fam_name, fam_id, r)
+        # self.get_biological_process_or_activity_by_gene_family(fam_name, fam_id, r)
+        # self.get_pathway_by_gene_family(fam_name, fam_id, r)
 
         self.logger.debug(f'Parsing data file complete.')
 
@@ -241,7 +325,6 @@ class PLoader(SourceDataLoader):
         # create the edge
         self.final_edge_list.append({})
 
-
     def get_cellular_component_by_gene_family(self):
         # create the gene node
         self.final_node_list.append({'id': '', 'name': '', 'properties': {}})
@@ -259,6 +342,7 @@ class PLoader(SourceDataLoader):
         self.final_node_list.append({'id': '', 'name': '', 'properties': {}})
 
         pass
+
 
 if __name__ == '__main__':
     # create a command line parser
