91a56b8de03c95129a669ba46c3944b1322bdc28
Evan Morris <evandietzmorris@gmail.com>
2021-07-30 00:21:40 -0400

    removing DATA_SERVICES_RESOURCES env and other unused env vars

--- set_up_test_env.sh
+++ set_up_test_env.sh
@@ -7,8 +7,6 @@ export DATA_SERVICES_STORAGE=$PWD/../Data_services_storage/
 cp $PWD/default-sources-spec.yml $PWD/../Data_services_storage/sources-spec.yml
 export DATA_SERVICES_SOURCES_SPEC=sources-spec.yml
 
-export DATA_SERVICES_RESOURCES=$PWD/tests/resources/
-
 mkdir -p $PWD/../Data_services_graphs
 export DATA_SERVICES_GRAPHS=$PWD/../Data_services_graphs/
 
