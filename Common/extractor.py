2ba46642d563a3e70c1b065e370703a82aad20e7
Evan Morris <evandietzmorris@gmail.com>
2021-07-30 00:55:55 -0400

    added json_extract, added error tracking for parsing exceptions, prevent adding nodes with null ids

--- Common/extractor.py
+++ Common/extractor.py
@@ -16,15 +16,17 @@ class Extractor:
         self.nodes = []
         self.edges = []
 
-        self.load_metadata = { 'record_counter': 0, 'skipped_record_counter': 0 }
+        self.load_metadata = { 'record_counter': 0, 'skipped_record_counter': 0, 'errors': []}
+        self.errors = []
 
-    def csv_extract(self, infile, subject_extractor,
-                   object_extractor,
-                   predicate_extractor,
-                   subject_property_extractor,
-                   object_property_extractor,
-                   edge_property_extractor,
-                   comment_character="#",delim='\t'):
+    def csv_extract(self, infile,
+                    subject_extractor,
+                    object_extractor,
+                    predicate_extractor,
+                    subject_property_extractor,
+                    object_property_extractor,
+                    edge_property_extractor,
+                    comment_character="#",delim='\t'):
         """Read a csv, perform callbacks to retrieve node and edge info per row.
         Assumes that all of the properties extractable for a node occur on the line with the node identifier"""
         for line in infile:
@@ -36,6 +38,7 @@ class Extractor:
                 x = line[:-1].split(delim)
                 self.parse_row(x, subject_extractor, object_extractor, predicate_extractor, subject_property_extractor, object_property_extractor, edge_property_extractor)
             except Exception as e:
+                self.load_metadata['errors'].append(e.__str__())
                 self.load_metadata['skipped_record_counter'] += 1
 
     def sql_extract(self, cursor, sql_query, subject_extractor, object_extractor, predicate_extractor, subject_property_extractor, object_property_extractor, edge_property_extractor):
@@ -49,7 +52,25 @@ class Extractor:
             try:
                 self.parse_row(row, subject_extractor, object_extractor, predicate_extractor, subject_property_extractor, object_property_extractor, edge_property_extractor)
             except Exception as e:
+                self.load_metadata['errors'].append(e.__str__())
                 self.load_metadata['skipped_record_counter'] += 1
+
+    def json_extract(self,
+                     json_array,
+                     subject_extractor,
+                     object_extractor,
+                     predicate_extractor,
+                     subject_property_extractor,
+                     object_property_extractor,
+                     edge_property_extractor):
+        for item in json_array:
+            self.load_metadata['record_counter'] += 1
+            try:
+                self.parse_row(item, subject_extractor, object_extractor, predicate_extractor, subject_property_extractor, object_property_extractor, edge_property_extractor)
+            except Exception as e:
+                self.load_metadata['errors'].append(e.__str__())
+                self.load_metadata['skipped_record_counter'] += 1
+                return
 
     def parse_row(self, row, subject_extractor, object_extractor, predicate_extractor, subject_property_extractor, object_property_extractor, edge_property_extractor):
         # pull the information out of the edge
@@ -61,31 +82,33 @@ class Extractor:
         edgeprops = edge_property_extractor(row)
 
         # if we  haven't seen the subject before, add it to nodes
-        if subject_id not in self.node_ids:
+        if subject_id and subject_id not in self.node_ids:
             subject_name = subjectprops.pop('name', None)
             subject_categories = subjectprops.pop('categories', None)
             subject_node = kgxnode(subject_id, name=subject_name, categories=subject_categories, nodeprops=subjectprops)
             self.nodes.append(subject_node)
             self.node_ids.add(subject_id)
 
-        # if we  haven't seen the subject before, add it to nodes
-        if object_id not in self.node_ids:
+        # if we  haven't seen the object before, add it to nodes
+        if object_id and object_id not in self.node_ids:
             object_name = objectprops.pop('name', None)
             object_categories = objectprops.pop('categories', None)
             object_node = kgxnode(object_id, name=object_name, categories=object_categories, nodeprops=objectprops)
             self.nodes.append(object_node)
             self.node_ids.add(object_id)
 
-        original_knowledge_source = edgeprops.pop(ORIGINAL_KNOWLEDGE_SOURCE, None)
-        primary_knowledge_source = edgeprops.pop(PRIMARY_KNOWLEDGE_SOURCE, None)
-        aggregator_knowledge_sources = edgeprops.pop(AGGREGATOR_KNOWLEDGE_SOURCES, None)
-        edge = kgxedge(subject_id,
-                       object_id,
-                       relation=predicate,
-                       predicate=predicate,
-                       original_knowledge_source=original_knowledge_source,
-                       primary_knowledge_source=primary_knowledge_source,
-                       aggregator_knowledge_sources=aggregator_knowledge_sources,
-                       edgeprops=edgeprops)
-        self.edges.append(edge)
-
+        if subject_id and object_id and predicate:
+            original_knowledge_source = edgeprops.pop(ORIGINAL_KNOWLEDGE_SOURCE, None)
+            primary_knowledge_source = edgeprops.pop(PRIMARY_KNOWLEDGE_SOURCE, None)
+            aggregator_knowledge_sources = edgeprops.pop(AGGREGATOR_KNOWLEDGE_SOURCES, None)
+            edge = kgxedge(subject_id,
+                           object_id,
+                           relation=predicate,
+                           predicate=predicate,
+                           original_knowledge_source=original_knowledge_source,
+                           primary_knowledge_source=primary_knowledge_source,
+                           aggregator_knowledge_sources=aggregator_knowledge_sources,
+                           edgeprops=edgeprops)
+            self.edges.append(edge)
+        else:
+            self.load_metadata['skipped_record_counter'] += 1
