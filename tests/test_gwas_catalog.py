1e2839ce3c9c6a8082a1754de95033d0b9b60e3b
Evan Morris <evandietzmorris@gmail.com>
2021-08-27 03:32:18 -0400

    fixing test.. that is not an int anymore it's a string

--- tests/test_gwas_catalog.py
+++ tests/test_gwas_catalog.py
@@ -7,7 +7,7 @@ def test_gwas_catalog_update_version():
     # ideally we'd mock up a fake gwas catalog FTP
     g_cat = GWASCatalogLoader()
     latest_version = g_cat.get_latest_source_version()
-    assert int(latest_version)
+    assert latest_version
 
 
 def not_active_test_gwas_catalog_load():
