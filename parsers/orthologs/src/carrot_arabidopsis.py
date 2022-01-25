05b758881795e7bbc44691d6747afa876acff7c2
Shalsh <shrivastava.shal@gmail.com>
2022-01-25 16:22:53 -0500

    Debugging module not found error

--- parsers/orthologs/src/carrot_arabidopsis.py
+++ parsers/orthologs/src/carrot_arabidopsis.py
@@ -2,50 +2,51 @@ import argparse
 import os
 
 from Common.loader_interface import SourceDataLoader
-from Common.utils import GetData
+# from Common.utils import GetData
 
 
-class CarAraOrthLoader(SourceDataLoader):
+# class CarrotArabidopsisOrthlogsLoader(SourceDataLoader):
 
-    def __init__(self, test_mode: bool = False, source_data_dir: str = None):
-        """
-        :param test_mode - sets the run into test mode
-        :param source_data_dir - the specific storage directory to save files in
-        """
-        super().__init__(test_mode=test_mode, source_data_dir=source_data_dir)
-        data_file = "DCv3_Arabidopsis_orthologs.txt"
-        data_file_path = os.path.join(self.data_path, data_file)
+    # # def __init__(self, test_mode: bool = False, source_data_dir: str = None):
+    #     """
+    #     :param test_mode - sets the run into test mode
+    #     :param source_data_dir - the specific storage directory to save files in
+    #     """
+        # super().__init__(test_mode=test_mode, source_data_dir=source_data_dir)
+        # self.data_file = "DCv3_Arabidopsis_orthologs.txt"
+        # self.data_file_path = os.path.join(self.data_path, self.data_file)
 
 
     # get latest source version
-    def get_latest_source_version(self):
-        """
-        gets the version of the data
-
-        :return:
-        """
-        # init the return
-        ret_val: str = 'Not needed, locally stored file.'
-        return ret_val
+    # def get_latest_source_version(self):
+    #     """
+    #     gets the version of the data
+    #
+    #     :return:
+    #     """
+    #     # init the return
+    #     ret_val: str = 'Not needed, locally stored file.'
+    #     return ret_val
 
     # get data
-    def get_data(self) -> int:
-        """
-        Locally stored file
-        Download not needed
-        :return:
-        """
-        # To Do: Store it somewhere and download from that url
-        ret_val = 1
-        return ret_val
+    # def get_data(self) -> int:
+    #     """
+    #     Locally stored file
+    #     Download not needed
+    #     :return:
+    #     """
+    #     # To Do: Store it somewhere and download from that url
+    #     ret_val = 1
+    #     return ret_val
 
     # parse data
-    def parse_data(self) -> dict:
-        """
-        Parses the data file for nodes/edges
-
-        :return: dict of parsing metadata results
-        """
+    # def parse_data(self) -> dict:
+    #     """
+    #     Parses the data file for nodes/edges
+    #
+    #     :return: dict of parsing metadata results
+    #     """
+    #     print(self.data_file_path)
 
 
 
@@ -63,10 +64,10 @@ if __name__ == '__main__':
     # get the params
     data_dir = args['data_dir']
 
-    carOrtholog = CarAraOrthLoader(False)
+    # orthologLoader = CarrotArabidopsisOrthlogsLoader(False)
 
     # load data files and create KGX output
-    carOrtholog.load(f"{data_dir}/nodes", f"{data_dir}/edges")
+    # orthologLoader.load(f"{data_dir}/nodes", f"{data_dir}/edges")
 
 
 
