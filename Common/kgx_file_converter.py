367e3eacbf9e952835240e826612a25ab6c68a59
Evan Morris <evandietzmorris@gmail.com>
2022-05-04 13:12:52 -0400

    improved property type recognition and error handling, added enforcement of basic rules like no None values

--- Common/kgx_file_converter.py
+++ Common/kgx_file_converter.py
@@ -1,20 +1,59 @@
 import csv
 import argparse
+from collections import defaultdict
 from Common.utils import quick_jsonl_file_iterator
 from Common.node_types import SUBJECT_ID, OBJECT_ID, PREDICATE
-
 # from kgx.transformer import Transformer
 
 
+def convert_jsonl_to_neo4j_csv(nodes_input_file: str,
+                               edges_input_file: str,
+                               nodes_output_file: str = None,
+                               edges_output_file: str = None,
+                               output_delimiter='\t',
+                               array_delimiter=chr(31)):  # chr(31) = U+001F - Unit Separator
+
+    if not nodes_output_file:
+        nodes_output_file = f'{nodes_input_file.rsplit(".")[0]}.csv'
+    if not edges_output_file:
+        edges_output_file = f'{edges_input_file.rsplit(".")[0]}.csv'
+
+    required_node_properties = {
+        'id': 'ID',
+        'name': 'string',
+        'category': 'LABEL'
+    }
+    node_properties = __determine_properties_and_types(nodes_input_file, required_node_properties)
+    __convert_to_csv(input_file=nodes_input_file,
+                     output_file=nodes_output_file,
+                     properties=node_properties,
+                     output_delimiter=output_delimiter,
+                     array_delimiter=array_delimiter)
+    __verify_conversion(nodes_output_file, node_properties, array_delimiter, output_delimiter)
+
+    required_edge_properties = {
+        SUBJECT_ID: 'START_ID',
+        PREDICATE: 'TYPE',
+        OBJECT_ID: 'END_ID'
+    }
+    edge_properties = __determine_properties_and_types(edges_input_file, required_edge_properties)
+    __convert_to_csv(input_file=edges_input_file,
+                     output_file=edges_output_file,
+                     properties=edge_properties,
+                     output_delimiter=output_delimiter,
+                     array_delimiter=array_delimiter)
+    __verify_conversion(edges_output_file, edge_properties, array_delimiter, output_delimiter)
+
+
 def __verify_conversion(file_path: str,
-                        property_types: dict,
+                        properties: dict,
                         array_delimiter: str,
                         output_delimiter: str):
     counter = 0
-    list_properties = [prop for prop in property_types
-                       if property_types[prop] == "string[]" or property_types[prop] == 'LABEL']
+    list_properties = [prop for prop in properties
+                       if properties[prop] == "string[]" or properties[prop] == 'LABEL']
     verified_properties = set()
-    num_properties = len(property_types.keys())
+    num_properties = len(properties.keys())
     with open(file_path, newline='') as file_handler:
         csv_reader = csv.reader(file_handler, delimiter=output_delimiter)
         next(csv_reader)
@@ -23,15 +62,15 @@ def __verify_conversion(file_path: str,
             if len(split_line) != num_properties:
                 raise Exception(f'Number of fields mismatch on line {counter} - got {len(split_line)}'
                                 f' expected {len(property_types)}: {line}')
-            for (prop, value) in zip(property_types.keys(), split_line):
+            for (prop, value) in zip(properties.keys(), split_line):
                 if not value:
                     continue
                 if prop in list_properties:
-                    attempted_list_unpack = value.split(array_delimiter)
+                    value.split(array_delimiter)
                     verified_properties.add(prop)
                 elif array_delimiter in value:
                     raise Exception(f'Array delimiter found in a non-list field - property {prop}: {value}')
-                elif property_types[prop] == 'float':
+                elif properties[prop] == 'float':
                     float(value)
                     verified_properties.add(prop)
                 else:
@@ -45,30 +84,66 @@ def __verify_conversion(file_path: str,
         print(f'Passed verification step: {file_path}')
 
 
-def __determine_properties_and_types(file_path: str, properties: list, property_types: dict):
+def __determine_properties_and_types(file_path: str, required_properties: dict):
+    property_type_counts = defaultdict(lambda: defaultdict(int))
     for entity in quick_jsonl_file_iterator(file_path):
         for key, value in entity.items():
-            if key not in properties:
-                properties.append(key)
-                if isinstance(value, int) or isinstance(value, float):
-                    property_types[key] = "double"
-                elif isinstance(value, list):
-                    property_types[key] = "string[]"
-                elif isinstance(value, bool):
-                    property_types[key] = "boolean"
-                else:
-                    property_types[key] = "string"
-    print(f'Found {len(properties)} properties:{properties}')
+            if value is None:
+                property_type_counts[key]["None"] += 1
+                if key in required_properties:
+                    print(f'WARNING: Required property None: {entity.items()}')
+            elif isinstance(value, bool):
+                property_type_counts[key]["boolean"] += 1
+            elif isinstance(value, int):
+                property_type_counts[key]["int"] += 1
+            elif isinstance(value, float):
+                property_type_counts[key]["float"] += 1
+            elif isinstance(value, list):
+                property_type_counts[key]["string[]"] += 1
+            else:
+                property_type_counts[key]["string"] += 1
+
+    properties = required_properties.copy()
+    for prop, type_counts in property_type_counts.items():
+        prop_types = list(type_counts.keys())
+        num_prop_types = len(prop_types)
+        has_type_conflicts = num_prop_types > 1
+
+        if 'None' in prop_types:
+            print(f'WARNING: None found as a value for property {prop}, that should not happen!')
+            if prop in required_properties:
+                raise Exception(f'None found as a value for a required property - {type_counts.items()}')
+
+        if prop in required_properties and has_type_conflicts:
+            raise Exception(f'Required property {prop} had multiple conflicting types: {type_counts.items()}')
+        elif not has_type_conflicts:
+            properties[prop] = prop_types[0]
+        else:
+            if 'string[]' in prop_types:
+                properties[prop] = 'string[]'
+            elif 'float' in prop_types and 'int' in prop_types and num_prop_types == 2:
+                properties[prop] = 'float'
+            elif 'float' in prop_types and 'None' in prop_types and num_prop_types == 2:
+                properties[prop] = 'float'
+            elif 'int' in prop_types and 'None' in prop_types and num_prop_types == 2:
+                properties[prop] = 'int'
+            else:
+                properties[prop] = 'string'
+
+        if prop not in properties:
+            raise Exception(f'Property type could not be determined for: {prop}. {type_counts.items()}')
+
+    print(f'Found {len(properties)} properties:{properties.items()}')
+    return properties
 
 
 def __convert_to_csv(input_file: str,
                      output_file: str,
-                     properties: list,
-                     property_types: dict,
+                     properties: dict,  # dictionary of { node/edge property: property_type }
                      array_delimiter: str,
                      output_delimiter: str):
 
-    headers = {prop: f'{prop}:{property_types[prop]}' for prop in properties}
+    headers = {prop: f'{prop}:{prop_type}' for prop, prop_type in properties.items()}
     with open(output_file, 'w', newline='') as output_file_handler:
         csv_file_writer = csv.DictWriter(output_file_handler,
                                          delimiter=output_delimiter,
@@ -76,79 +151,21 @@ def __convert_to_csv(input_file: str,
                                          restval='',
                                          quoting=csv.QUOTE_MINIMAL)
         csv_file_writer.writerow(headers)
-        for node in quick_jsonl_file_iterator(input_file):
-            for prop, prop_type in property_types.items():
-                if prop_type == 'string[]' or prop_type == 'LABEL':
-                    if prop in node:
+        for item in quick_jsonl_file_iterator(input_file):
+            for key in list(item.keys()):
+                if item[key] is None:
+                    del item[key]
+                else:
+                    prop_type = properties[key]
+                    if prop_type == 'string[]' or prop_type == 'LABEL':
                         # convert lists into strings with an array delimiter
-                        node[prop] = array_delimiter.join(node[prop])
-                elif prop_type == 'boolean':
-                    if prop in node:
+                        item[key] = array_delimiter.join(item[key])
+                    elif prop_type == 'boolean':
                         # neo4j handles boolean with string 'true' being true and everything else false
-                        node[prop] = 'true' if node[prop] is True else 'false'
-            csv_file_writer.writerow(node)
+                        item[key] = 'true' if item[key] is True else 'false'
+            csv_file_writer.writerow(item)
 
 
-def convert_jsonl_to_neo4j_csv(nodes_input_file: str,
-                               edges_input_file: str,
-                               nodes_output_file: str = None,
-                               edges_output_file: str = None):
-
-    output_delimiter = '\t'
-    array_delimiter = chr(31)  # U+001F - Unit Separator
-
-    if not nodes_output_file:
-        nodes_output_file = f'{nodes_input_file.rsplit(".")[0]}.csv'
-    if not edges_output_file:
-        edges_output_file = f'{edges_input_file.rsplit(".")[0]}.csv'
-
-    node_properties = ['id', 'name', 'category']
-    node_property_types = {
-        'id': 'ID',
-        'name': 'string',
-        'category': 'LABEL'
-    }
-    __determine_properties_and_types(nodes_input_file, node_properties, node_property_types)
-    __convert_to_csv(input_file=nodes_input_file,
-                     output_file=nodes_output_file,
-                     properties=node_properties,
-                     property_types=node_property_types,
-                     output_delimiter=output_delimiter,
-                     array_delimiter=array_delimiter)
-    __verify_conversion(nodes_output_file, node_property_types, array_delimiter, output_delimiter)
-
-    edge_properties = [SUBJECT_ID, PREDICATE, OBJECT_ID]
-    edge_property_types = {
-        SUBJECT_ID: 'START_ID',
-        PREDICATE: 'TYPE',
-        OBJECT_ID: 'END_ID'
-    }
-    __determine_properties_and_types(edges_input_file, edge_properties, edge_property_types)
-    __convert_to_csv(input_file=edges_input_file,
-                     output_file=edges_output_file,
-                     properties=edge_properties,
-                     property_types=edge_property_types,
-                     output_delimiter=output_delimiter,
-                     array_delimiter=array_delimiter)
-    __verify_conversion(edges_output_file, edge_property_types, array_delimiter, output_delimiter)
-
-    """
-
-    
-    # if we ever want to use KGX to do it
-    
-    input_args = {
-        'filename': [nodes_input_file, edges_input_file],
-        'format': 'jsonl'
-    }
-    output_args = {
-        'filename': nodes_output_file.rsplit(".")[0],
-        'list_delimiter': '\x31',
-        'format': 'tsv'
-    }
-    t = Transformer(stream=True)
-    t.transform(input_args, output_args)
-    """
 
 
 if __name__ == '__main__':
