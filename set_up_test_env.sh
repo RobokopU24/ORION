dc19a214ceb8d07ecf61708269967fe3177a5bac
Evan Morris <evandietzmorris@gmail.com>
2022-09-13 10:54:29 -0400

    preventing some path bugs

--- set_up_test_env.sh
+++ set_up_test_env.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-export PYTHONPATH=$PWD
 
 # These environment variables are required by Data Services. See the README for more information.
 #
@@ -8,16 +7,16 @@ export PYTHONPATH=$PWD
 # DATA_SERVICES_GRAPH_SPEC - a file where graphs to be built are specified
 # DATA_SERVICES_LOGS - a directory for storing logs
 
-mkdir -p $PWD/../Data_services_storage
+mkdir -p "$PWD"/../Data_services_storage
 export DATA_SERVICES_STORAGE=$PWD/../Data_services_storage/
 
-mkdir -p $PWD/../Data_services_graphs
+mkdir -p "$PWD"/../Data_services_graphs
 export DATA_SERVICES_GRAPHS=$PWD/../Data_services_graphs/
 
 export DATA_SERVICES_GRAPH_SPEC=testing-graph-spec.yml
 #export DATA_SERVICES_GRAPH_SPEC_URL=https://raw.githubusercontent.com/RENCI-AUTOMAT/Data_services/helm_deploy/graph_specs/yeast-graph-spec.yml
 
-mkdir -p $PWD/../Data_services_logs
+mkdir -p "$PWD"/../Data_services_logs
 export DATA_SERVICES_LOGS=$PWD/../Data_services_logs/
 
 export DATA_SERVICES_NEO4J_PASSWORD=insecurepasswordexample
