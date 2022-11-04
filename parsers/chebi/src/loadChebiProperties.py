aa104254ee6767ad0f84a89e296793fa9c1d4146
Evan Morris <evandietzmorris@gmail.com>
2022-11-04 00:11:39 -0400

    refactored to use constants and variable names that match the rest of the codebase better, moved some functions to the instance level instead of being defined within another function, simplified output node creation loop, removed 'restroles' as a property, used a set instead of a list to accumulate role ids (preventing duplicates)

--- parsers/chebi/src/loadChebiProperties.py
+++ parsers/chebi/src/loadChebiProperties.py
@@ -1,15 +1,24 @@
 import os
 import argparse
-import pyoxigraph
 from gzip import GzipFile
 
-from zipfile import ZipFile
 from collections import defaultdict
 from Common.utils import GetData
 from Common.loader_interface import SourceDataLoader
 from Common.kgxmodel import kgxnode
+from Common.prefixes import CHEBI
 
+RELATION_TYPE_COLUMN = 1
+RELATION_INIT_ID_COLUMN = 2
+RELATION_FINAL_ID_COLUMN = 3
 
+COMPOUNDS_CHEBI_ID_COLUMN = 2
+COMPOUNDS_CHEBI_NAME_COLUMN = 5
+
+CHEBI_ROLES_TO_IGNORE = ['CHEBI_ROLE:role',
+                         'CHEBI_ROLE:biological_role',
+                         'CHEBI_ROLE:chemical_role',
+                         'CHEBI_ROLE:application']
 
 ##############
 # Class: Chebi-Properties loader
@@ -18,12 +27,12 @@ from Common.kgxmodel import kgxnode
 # Date: 10/19/2022
 # Desc: Class that loads/parses the Chebi-Properties data.
 ##############
-class ChebiSuplementLoader(SourceDataLoader):
+class ChebiPropertiesLoader(SourceDataLoader):
 
     # Setting the class level variables for the source ID and provenance
     source_id: str = 'ChebiProperties'
     provenance_id: str = 'infores:chebi-properties'
-    
+    parsing_version = '1.0'
 
     def __init__(self, test_mode: bool = False, source_data_dir: str = None):
         """
@@ -33,10 +42,11 @@ class ChebiSuplementLoader(SourceDataLoader):
         super().__init__(test_mode=test_mode, source_data_dir=source_data_dir)
 
         self.data_url: str = 'https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited/'
-        self.data_file: str = 'compounds.tsv.gz'
-        self.relation: str = 'relation.tsv'
-        self.data_files = [self.data_file,
-                            self.relation]
+        self.compounds_file: str = 'compounds.tsv.gz'
+        self.relation_file: str = 'relation.tsv'
+        self.data_files = [self.compounds_file,
+                           self.relation_file]
+
     def get_latest_source_version(self) -> str:
         """
         gets the latest available version of the data
@@ -44,7 +54,7 @@ class ChebiSuplementLoader(SourceDataLoader):
         :return:
         """
         #Get the version for the compounds dataset
-        file_url = f'{self.data_url}{self.data_file}'
+        file_url = f'{self.data_url}{self.compounds_file}'
         gd = GetData(self.logger.level)
         latest_source_version = gd.get_http_file_modified_date(file_url)
         return latest_source_version
@@ -58,101 +68,61 @@ class ChebiSuplementLoader(SourceDataLoader):
         gd: GetData = GetData(self.logger.level)
         for dt_file in self.data_files:
             gd.pull_via_http(f'{self.data_url}{dt_file}',
-                                           self.data_path)
+                             self.data_path)
         return True
 
     def parse_data(self):
         """
         Parses the data file for graph nodes/edges
         """
-        def fixname(n):
-            return f'CHEBI_ROLE:{"_".join(n.split())}'
-        
-        def update_ancestors(ancestors,parent,iia):
-            kids = iia[parent]
-            for kid in kids:
-                ancestors[kid].append(parent)
-                ancestors[kid] += ancestors[parent]
-                update_ancestors(ancestors,kid,iia)
 
-        def get_ancestors(iia):
-            ancestors = defaultdict(list)
-            role = 'CHEBI:50906'
-            update_ancestors(ancestors,role,iia)
-            print('CHEBI:50904 Ancestors', ancestors['CHEBI:50904'])
-            return ancestors
+        chebi_roles = self.read_roles()
 
-        def read_roles():
-            """This format is not completely obvious, but the triple is (FINAL_ID)-[type]->(INIT_ID)."""
-            roles = defaultdict(list)
-            invert_is_a = defaultdict(list)
-            relation_file_path = os.path.join(self.data_path, f'{self.relation}')
-            with open(relation_file_path,'r') as inf:
-                for line in inf:
-                    x = line.strip().split('\t')
-                    if x[1] == 'has_role':
-                        roles[f'CHEBI:{x[3]}'].append(f'CHEBI:{x[2]}')
-                    elif x[1] == 'is_a':
-                        child = f'CHEBI:{x[3]}'
-                        parent = f'CHEBI:{x[2]}'
-                        invert_is_a[parent].append(child)
-            #Now include parents
-            ancestors = get_ancestors(invert_is_a)
-            for node,noderoles in roles.items():
-                if node == 'CHEBI:64663':
-                    print('hi checking CHEBI:64663 ')
-                restroles= []
-                for role in noderoles:
-                    moreroles=ancestors[role]
-                    restroles += moreroles
-                roles[node] += restroles
-            return roles
+        # iterate through the compounds file and create a dictionary of chebi_id -> name
+        names = {}
+        skipped_header = False
+        archive_file_path = os.path.join(self.data_path, self.compounds_file)
+        with GzipFile(archive_file_path) as zf:
+            for bytesline in zf:
+
+                # skip the header
+                if not skipped_header:
+                    skipped_header = True
+                    continue
+
+                lines = bytesline.decode('utf-8')
+                line = lines.strip().split('\t')
+                chebi_id = line[COMPOUNDS_CHEBI_ID_COLUMN]
+                cname = line[COMPOUNDS_CHEBI_NAME_COLUMN]
+                names[chebi_id] = cname
 
         # init the record counters
         record_counter: int = 0
         skipped_record_counter: int = 0
-        names = {}
-        chebi_roles = read_roles()
 
-        archive_file_path = os.path.join(self.data_path, f'{self.data_file}')
-        with GzipFile(archive_file_path) as zf:
-            for bytesline in zf:
+        # walk through the chebi IDs and names from the compounds file and create the chebi property nodes
+        for chebi_id, name in names.items():
 
-                # increment the record counter
-                record_counter += 1
+            # increment the record counter
+            record_counter += 1
+            if self.test_mode and record_counter == 2000:
+                break
 
-                if self.test_mode and record_counter == 2000:
-                    break
+            # convert the roles to properly formatted property names and filter out the ones in CHEBI_ROLES_TO_IGNORE
+            role_properties = [self.fixname(names[x]) for x in chebi_roles[chebi_id] if x not in CHEBI_ROLES_TO_IGNORE]
 
-                lines = bytesline.decode('utf-8')
-                line = lines.strip().split('\t')
-                cid = line[2]
-                cname = line[5]
-                names[cid] = cname
-
-        Chebi_nodes = []
-        for node_id, node in names.items():
-            if node == 'NAME':
-                continue
-            outnode = {'id': node_id, 'name': names[node_id],
-                    'rest_roles': [eid for eid in chebi_roles[node_id]]}
-            rolenames = [fixname(names[x]) for x in chebi_roles[node_id]]
-            for rn in rolenames:
-                if rn in ['CHEBI_ROLE:role', 'CHEBI_ROLE:biological_role', 'CHEBI_ROLE:chemical_role', 'CHEBI_ROLE:application']:
-                    continue
-                outnode[rn] = True
-            Chebi_nodes.append(outnode)
-        
-     
-        for chebi_node in Chebi_nodes:
-            # make sure the node list is not null
-            if chebi_node['name']!= 'null':
-                chebi_props = {k: chebi_node[k] for k in set(list(chebi_node.keys())) - set(['name', 'id'])}
-                node = kgxnode(chebi_node['id'], chebi_node['name'], nodeprops=chebi_props)
-                self.final_node_list.append(node)
-            else:
+            # only include nodes that have roles
+            if not role_properties:
                 skipped_record_counter += 1
-        
+            else:
+                # create a node with the properties
+                node_properties = {role: True for role in role_properties}
+                # node_properties['restroles'] = chebi_roles[chebi_id] - leaving in case we wanted this
+                output_node = kgxnode(chebi_id,
+                                      name=names[chebi_id],
+                                      nodeprops=node_properties)
+                self.final_node_list.append(output_node)
+
         self.logger.debug(f'Parsing data file complete.')
         # load up the metadata
         load_metadata: dict = {
@@ -160,9 +130,49 @@ class ChebiSuplementLoader(SourceDataLoader):
             'unusable_source_lines': skipped_record_counter
             }
 
-        # return the split file names so they can be removed if desired
         return load_metadata
 
+    def fixname(self, n):
+        return f'CHEBI_ROLE:{"_".join(n.split())}'
+
+    def update_ancestors(self, ancestors, parent, is_a_relationships):
+        kids = is_a_relationships[parent]
+        for kid in kids:
+            ancestors[kid].append(parent)
+            ancestors[kid] += ancestors[parent]
+            self.update_ancestors(ancestors, kid, is_a_relationships)
+
+    def get_ancestors(self, is_a_relationships):
+        ancestors = defaultdict(list)
+        role = 'CHEBI:50906'  # this is the "Eve" role, the ancestor with no parents: "role" itself
+        self.update_ancestors(ancestors, role, is_a_relationships)
+        # print('CHEBI:50904 Ancestors', ancestors['CHEBI:50904'])
+        return ancestors
+
+    def read_roles(self):
+        """This format is not completely obvious, but the triple is (FINAL_ID)-[type]->(INIT_ID)."""
+        roles = defaultdict(set)
+        is_a_relationships = defaultdict(list)
+        relation_file_path = os.path.join(self.data_path, self.relation_file)
+        with open(relation_file_path, 'r') as inf:
+            for line in inf:
+                x = line.strip().split('\t')
+                if x[RELATION_TYPE_COLUMN] == 'has_role':
+                    roles[f'{CHEBI}:{x[RELATION_FINAL_ID_COLUMN]}'].add(f'{CHEBI}:{x[RELATION_INIT_ID_COLUMN]}')
+                elif x[RELATION_TYPE_COLUMN] == 'is_a':
+                    child = f'{CHEBI}:{x[RELATION_FINAL_ID_COLUMN]}'
+                    parent = f'{CHEBI}:{x[RELATION_INIT_ID_COLUMN]}'
+                    is_a_relationships[parent].append(child)
+        # Now include parents
+        ancestors = self.get_ancestors(is_a_relationships)
+        for node, noderoles in roles.items():
+            # if node == 'CHEBI:64663':
+            #    print('hi checking CHEBI:64663 ')
+            ancestor_roles = []
+            for role in noderoles:
+                ancestor_roles += ancestors[role]
+            roles[node].update(ancestor_roles)
+        return roles
 
 if __name__ == '__main__':
     # create a command line parser
@@ -177,7 +187,7 @@ if __name__ == '__main__':
     ch_data_dir: str = args['data_dir']
 
     # get a reference to the processor
-    ch_ldr = ChebiSuplementLoader()
+    ch_ldr = ChebiPropertiesLoader()
 
     # load the data files and create KGX output
     ch_ldr.load(ch_data_dir + '/nodes.jsonl')
