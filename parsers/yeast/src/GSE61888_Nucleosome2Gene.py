a3c544d86b8a359cba3cc64a94f9fcdec9445d62
beasleyjonm <85600465+beasleyjonm@users.noreply.github.com>
2022-11-21 14:51:33 -0500

    Delete GSE61888_Nucleosome2Gene.py

--- parsers/yeast/src/GSE61888_Nucleosome2Gene.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-nuc_df = pd.read_csv('/SGD_Data_Storage/GSE61888_nucs_normed.csv')
-gene_df = pd.read_csv('/SGD_Data_Storage/SGDAllGenes.csv')
-
-new_nuc_df = nuc_df.merge(gene_df, left_on='acc', right_on='secondaryIdentifier')
-
-new_nuc_df.to_csv('/SGD_Data_Storage/GSE61888_nucs_normed_updated.csv', encoding="utf-8-sig", index=False)
