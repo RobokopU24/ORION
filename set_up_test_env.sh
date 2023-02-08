1a31fc9f78fc6c47bacc9f1dbae2484e38a9553d
Evan Morris <evandietzmorris@gmail.com>
2023-02-08 05:12:03 -0500

    fixing bug (finally) where spaces after comment characters would result in funky behavior

--- set_up_test_env.sh
+++ set_up_test_env.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-# These environment variables are required by Data Services. See the README for more information.
+#These environment variables are required by Data Services. See the README for more information.
 
-# DATA_SERVICES_STORAGE - a directory for storing data sources
+#DATA_SERVICES_STORAGE - a directory for storing data sources
 mkdir -p "$PWD/../Data_services_storage"
 export DATA_SERVICES_STORAGE="$PWD/../Data_services_storage/"
 
-# DATA_SERVICES_GRAPHS - a directory for storing knowledge graphs
-mkdir -p "$PWD/../Data_services_graphs"
+#DATA_SERVICES_GRAPHS - a directory for storing knowledge graphs
+#mkdir -p "$PWD/../Data_services_graphs"
 export DATA_SERVICES_GRAPHS="$PWD/../Data_services_graphs/"
 
-# DATA_SERVICES_LOGS - a directory for storing logs
+#DATA_SERVICES_LOGS - a directory for storing logs
 mkdir -p "$PWD/../Data_services_logs"
 export DATA_SERVICES_LOGS="$PWD/../Data_services_logs/"
 
-# Use EITHER of the following, DATA_SERVICES_GRAPH_SPEC or DATA_SERVICES_GRAPH_SPEC_URL
+#Use EITHER of the following, DATA_SERVICES_GRAPH_SPEC or DATA_SERVICES_GRAPH_SPEC_URL
 
-# DATA_SERVICES_GRAPH_SPEC - the name of a Graph Spec file located in the graph_specs directory of Data_services
+#DATA_SERVICES_GRAPH_SPEC - the name of a Graph Spec file located in the graph_specs directory of Data_services
 export DATA_SERVICES_GRAPH_SPEC=testing-graph-spec.yml
 
-# DATA_SERVICES_GRAPH_SPEC_URL - a URL pointing to a Graph Spec file
+#DATA_SERVICES_GRAPH_SPEC_URL - a URL pointing to a Graph Spec file
 #export DATA_SERVICES_GRAPH_SPEC_URL=https://raw.githubusercontent.com/RENCI-AUTOMAT/Data_services/helm_deploy/graph_specs/yeast-graph-spec.yml
 
 export DATA_SERVICES_NEO4J_PASSWORD=insecurepasswordexample
