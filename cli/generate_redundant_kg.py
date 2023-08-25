e92b623a4b0d7a509fd53e75c09b7d301f89ad46
James Chung <jchung@renci.org>
2023-08-25 14:56:27 -0400

    First version of redundant graph generator

---
+++ cli/generate_redundant_kg.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import re
+import argparse
+from itertools import product
+from Common.utils import quick_jsonl_file_iterator
+from Common.kgx_file_writer import KGXFileWriter
+
+from bmt import Toolkit
+
+
+bmt = Toolkit()
+
+
+QUALIFIER_KEYS = ['object_aspect_qualifier', 'object_direction_qualifier', 'qualified_predicate', 'species_context_qualifier']
+ASPECT_QUALIFIER = 'object_aspect_qualifier'
+DIRECTION_QUALIFIER = 'object_direction_qualifier'
+
+
+def get_ancestor_predicates_biolink(predicate):
+    cur_predicate = predicate.split(':')[-1]
+    return set(bmt.get_ancestors(cur_predicate))
+
+def check_qualifier(ed):
+    qfs = []
+    for k in ed.keys():
+        if bmt.is_qualifier(k):
+            qfs.append(k)
+    return qfs
+
+def write_edge_no_q(ed, predicate):
+    tmp_edge = dict(ed) # Not sure if it's still tied to original edge dictionary
+    tmp_edge['predicate'] = predicate
+    tmp_edge.pop(DIRECTION_QUALIFIER, None)
+    tmp_edge.pop(ASPECT_QUALIFIER, None)
+    return tmp_edge
+
+def generate_redundant_kg(infile, edges_file_path):
+    
+    num_edges = 0
+    num_bio_edges = 0
+    num_other_edges = 0
+    non_biolink_predicates = set()
+    with KGXFileWriter(edges_output_file_path=edges_file_path) as kgx_file_writer:
+        for edge in quick_jsonl_file_iterator(infile):
+            ancestor_predicates = set()
+            # qual_predicate = 'No_qualified_predicate'
+            aspect_values = []
+            direction_values = [None]
+            try:
+                num_edges +=1
+                if re.match('biolink.*', edge['predicate']):
+                    ancestor_predicates = ancestor_predicates.union(get_ancestor_predicates_biolink(edge['predicate']))
+                    num_bio_edges+=1
+                    
+                else:   #(f"predicate does not have biolink prefix: {edge['predicate']}")
+                    non_biolink_predicates.add(edge['predicate']) 
+                    num_other_edges+=1
+                    
+                qualifiers = check_qualifier(edge)
+                
+                if ASPECT_QUALIFIER in qualifiers:
+                    aspect_values += bmt.get_permissible_value_ancestors(permissible_value=edge[ASPECT_QUALIFIER], enum_name='GeneOrGeneProductOrChemicalEntityAspectEnum')
+                    if DIRECTION_QUALIFIER in qualifiers:
+                        # store tuples that represent all combinations of aspect_qualifier and direction_qualifier
+                        direction_values = [edge[DIRECTION_QUALIFIER], None]
+
+                
+                # Log the uncharted qualifiers here. Ignored, save for future investigation
+                #for k in qualifiers:
+                #    if k not in QUALIFIER_KEYS:
+                #        print(k)
+    
+                expand_edges = []
+
+                for cur_pre in ancestor_predicates:
+                    
+                    if (cur_pre == edge['predicate'].strip('biolink:')) and (len(aspect_values) >0): # There might be a function in bmt that retrieves normalized predicate names that could be updated here.
+                        # run tuples and change q_edge, will need another copy to enable popping key:value pairs of qualifiers
+                        for (a,d) in product(aspect_values, direction_values):
+                            q_edge = dict(edge)
+                            if d == None:
+                                q_edge.pop(DIRECTION_QUALIFIER, None)
+                            else:
+                                q_edge[DIRECTION_QUALIFIER] = d
+                                
+                            q_edge[ASPECT_QUALIFIER] = a
+                            expand_edges.append(q_edge)
+                    
+                    expand_edges.append(write_edge_no_q(edge, cur_pre))
+                    
+                            
+                #expand_edges.append(dict()) # Just to separate inoput instance for verification purpose. Should be deleted at production runs.
+                kgx_file_writer.write_normalized_edges(iter(expand_edges))
+                
+                
+            except KeyError:
+                print("There is no key named predicate in the dictionary. This does not look like an edge object")
+                
+
+        
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='Generate redundant edge files. '
+                                             'currently expanding from predicate and qualified_predicate.')
+    ap.add_argument('--infile', help='Input edge file path')
+    ap.add_argument('--outfile', help='Output edge file path')
+    args = vars(ap.parse_args())
+
+    infile =  args['infile'] #"/home/Documents/RENCI/ROBOKOP/generate_redundant_graphs/data/edges.jsonl.10"
+    edges_file_path = args['outfile']  #"/home/Documents/RENCI/ROBOKOP/generate_redundant_graphs/data/edges.redundant.jsonl.10"
+    
+    generate_redundant_kg(infile, edges_file_path)
