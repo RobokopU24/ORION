6bc0270ef8a66cc7be6b5eb231c5e95ac2d691ba
Evan Morris <evandietzmorris@gmail.com>
2024-07-19 16:44:32 -0400

    Squashed commit of the following:
    commit 1f01b43a842706e9cb5723c88c3bbd8371ff505a
    Merge: ec6b7d8 734cc6e
    Author: Evan Morris <evandietzmorris@gmail.com>
    Date:   Fri Jul 19 14:38:15 2024 -0400
    
        Merge pull request #232 from RobokopU24/binding-predicate-update
    
        Modified predicates affected by biolink:binds
    
    commit 734cc6e4c42e2315b217e762bb481aff867835e7
    Author: Evan Morris <evandietzmorris@gmail.com>
    Date:   Fri Jul 19 14:32:02 2024 -0400
    
        bumping parsing versions for binds refactor parsers
    
    commit ec6b7d8a7bfdfd17ec6b95354b21f17ce9079c75
    Merge: e44cae6 00c6627
    Author: Evan Morris <evandietzmorris@gmail.com>
    Date:   Fri Jul 19 14:28:50 2024 -0400
    
        Merge pull request #233 from RobokopU24/pharos_KL/AT
    
        Improving the KL/AT in Pharos
    
    commit 00c6627485c2866d7bbfc301db55ce428bcdcf1f
    Author: Evan Morris <evandietzmorris@gmail.com>
    Date:   Fri Jul 19 14:26:48 2024 -0400
    
        bumping parsing version
    
    commit 766c5b75dff428030bb2820547c1a3fb1521d248
    Author: eKathleenCarter <163005214+eKathleenCarter@users.noreply.github.com>
    Date:   Tue Jul 16 12:42:45 2024 -0400
    
        Update predicates.py
    
        forgot to revert changes for interacts_with. Will keep it unmodified until further clarification.
    
    commit 4cb0188743f17168cf5e5166196bf61e52ffd82d
    Author: eKathleenCarter <163005214+eKathleenCarter@users.noreply.github.com>
    Date:   Tue Jul 16 12:41:36 2024 -0400
    
        Update predicates.py after comments
    
        Made changes to reflect comments by Chris:
    
        1. Do not modify "ed50": f"RO:0002434" until we know where this predicate comes from.
        "But I guess the main point is that it makes no sense to have an ed50 between a chemical and a gene/protein. Is it possible to see whether we actually have any of these?"
    
        2. "gi50": f"RO:0002434" -> {DGIDB}:Inhibitor
    
        3. Do not modify "interacts_with"  until we know where this predicate comes from.
        I" guess it would be easier to be sure if we know the context in which "interacts-with" was found in our ingestss.
    
        (but I'd be tempted to put 2436 for this one)"
    
        Main take away: where do these predicates come from? Can we track down how this was generated?
    
    commit 711ac5af9d19ae6bdeb7c24c1b54dd836db3cfa5
    Author: Kathleen <ekcarter@renci.org>
    Date:   Wed Jul 10 15:10:28 2024 -0400
    
        Improving the KL/AT in Phaors
    
    commit a687f3066bd7a1fb8a17897d2b7805f546f9aa57
    Author: Kathleen <ekcarter@renci.org>
    Date:   Wed Jul 10 12:32:26 2024 -0400
    
        Modified predicates affected by biolink:binds
    
    commit e44cae6e8ff2bcff2ce08a57a8749997241107a1
    Merge: 7ba9538 fa8ebf0
    Author: Evan Morris <evandietzmorris@gmail.com>
    Date:   Wed Jun 26 16:38:02 2024 -0400
    
        Merge pull request #231 from RobokopU24/requirements.txt-patch
    
        Update requirements.txt
    
    commit fa8ebf05e3761cb0d563818928857d0c8af3a2d2
    Author: beasleyjonm <85600465+beasleyjonm@users.noreply.github.com>
    Date:   Wed Jun 26 16:37:03 2024 -0400
    
        Update requirements.txt
    
        Updated requests 2.32.0 -> 2.32.3

--- Common/predicates.py
+++ Common/predicates.py
@@ -8,43 +8,43 @@ DGIDB_PREDICATE_MAPPING = {
     "allosteric_antagonist": f"{DGIDB}:antagonist",
     "allosteric_modulator": f"{DGIDB}:modulator",
     "antagonist": f"{DGIDB}:antagonist",
-    "antibody": f"{DGIDB}:binder",
-    "antibody_binding": f"{DGIDB}:binder",
+    "antibody": f"RO:0002436",
+    "antibody_binding": f"RO:0002436",
     "antisense_inhibitor": f"{DGIDB}:inhibitor",
-    "app_ki": f"RO:0002434",
-    "app_km": f"RO:0002434",
-    "binding_agent": f"{DGIDB}:binder",
+    "app_ki": f"RO:0002434",  # apparent Ki?  if so change to RO:0002436
+    "app_km": f"RO:0002434",  # apperent Km?  if so change to RO:0002436
+    "binding_agent": f"RO:0002436",
     "blocker": f"{DGIDB}:blocker",
     "channel_blocker": f"{DGIDB}:channel_blocker",
     "ec50": f"{DGIDB}:agonist",
-    "ed50": f"RO:0002434",
+    "ed50": f"RO:0002434",  # Effective Dose. Where does this predicate come from? CB (2024_07): "it makes no sense to have an ed50 between a chemical and a gene/protein"
     "gating_inhibitor": f"{DGIDB}:gating_inhibitor",
-    "gi50": f"RO:0002434",
+    "gi50": f"{DGIDB}:Inhibitor",  # Growth Inhibitor
     "ic50": f"{DGIDB}:inhibitor",
     "inhibitor": f"{DGIDB}:inhibitor",
-    "interacts_with": f"RO:0002434",
+    "interacts_with": f"RO:0002434",  # Where does this predicate come from? Possiblely needs to be modified to RO:0002436
     "inverse_agonist": f"{DGIDB}:inverse_agonist",
-    "ka": f"RO:0002434",
-    "kact": f"RO:0002434",
-    "kb": f"{DGIDB}:binder",
-    "kd": f"{DGIDB}:binder",
-    "kd1": f"RO:0002434",
+    "ka": f"RO:0002436",
+    "kact": f"RO:0002436",  # is this a miss type of kcat?
+    "kb": f"RO:0002436",  # {DGIDB}:binder maps to biolink:binds which is depreciated
+    "kd": f"RO:0002436",
+    "kd1": f"RO:0002436",  # RO:0002434 maps to biolink:related_to
     "ki": f"{DGIDB}:inhibitor",
-    "km": f"RO:0002434",
-    "ks": f"RO:0002434",
+    "km": f"RO:0002436",
+    "ks": f"RO:0002436",
     "modulator": f"{DGIDB}:modulator",
-    "mic": f"RO:0002434",
-    "mpc": f"RO:0002434",
+    "mic": f"RO:0002434",  # What is this referring to?
+    "mpc": f"RO:0002434",  # What is this referring to?
     "negative_modulator": f"{CHEMBL_MECHANISM}:negative_modulator",
     "negative_allosteric_modulator": f"{CHEMBL_MECHANISM}:negative_modulator",
     "opener": f"{CHEMBL_MECHANISM}:opener",
     "other": f"{DGIDB}:other",
     "partial_agonist": f"{DGIDB}:partial_agonist",
-    "pa2": f"RO:0002434",
+    "pa2": f"RO:0002434",  # What is this referring to?
     "pharmacological_chaperone": f"{DGIDB}:chaperone",
     "positive_allosteric_modulator": f"{CHEMBL_MECHANISM}:positive_modulator",
     "positive_modulator": f"{CHEMBL_MECHANISM}:positive_modulator",
     "releasing_agent": f"{CHEMBL_MECHANISM}:releasing_agent",
     "substrate": f"{CHEMBL_MECHANISM}:substrate",
-    "xc50": f"RO:0002434"
+    "xc50": f"RO:0002436"  # This is related to ec50 and ic50 both of which describe binding events
 }
