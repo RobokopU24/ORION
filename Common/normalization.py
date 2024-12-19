12edddc75000adcd0353a7ae7979587c74381fce
Evan Morris <evandietzmorris@gmail.com>
2024-12-19 13:29:44 -0500

    bumping normalization code version

--- Common/normalization.py
+++ Common/normalization.py
@@ -10,7 +10,7 @@ from robokop_genetics.genetics_normalization import GeneticsNormalizer
 from Common.biolink_constants import *
 from Common.utils import LoggingUtil
 
-NORMALIZATION_CODE_VERSION = '1.2'
+NORMALIZATION_CODE_VERSION = '1.3'
 
 # node property name for node types that did not normalize
 CUSTOM_NODE_TYPES = 'custom_node_types'
