4675e791d0837e0c876ff8233dd125f1d6bdd9aa
hyi <hongyi@renci.org>
2025-03-01 12:21:37 -0500

    get pipeline integration working locally

--- celery_worker.py
+++ celery_worker.py
@@ -6,12 +6,14 @@ import os
 # Configure Celery to connect to the Redis broker
 celery_app = Celery(
     "orion_worker",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0"
+    broker=os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0"),
+    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0"),
 )
 
 
 # Define the task handler for orion.data_ingestion
+# Make sure the name aligns with the task name used by celery in the upstream app
+# so that the data ingestion task can be picked up from the task queue
 @celery_app.task(name="orion.data_ingestion")
 def run_build_manager(task_data):
     try:
@@ -21,8 +23,9 @@ def run_build_manager(task_data):
         print(f'pwd_path: {pwd_path}', flush=True)
         print(f'task_data: {task_data}', flush=True)
         # Run build_manager.py as a subprocess with the provided config
+        os.environ["ORION_GRAPH_SPEC"] = task_data["graph_spec_filename"]
         result = subprocess.run(
-            ["python", "build_manager.py", "Example_Graph"],
+            ["python", "build_manager.py", task_data["graph_id"]],
             capture_output=True,
             text=True,
             check=True
