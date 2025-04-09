dab6e1782e37c9370464b8b91b8e9c7013a41fae
Bradford Powell <bpow@drpowell.org>
2025-04-09 14:23:29 -0400

    Add some defaults to json_extract
    This allows easier parsing if someone just so happens to have an array
    of objects with just the right key=>value pairs. Or if they are using
    a generator that modifies those items on the way in...
    
    This is designed to be fully backwards compatible.

--- Common/extractor.py
+++ Common/extractor.py
@@ -1,4 +1,3 @@
-import csv
 from Common.kgxmodel import kgxnode, kgxedge
 from Common.kgx_file_writer import KGXFileWriter
 from Common.biolink_constants import PRIMARY_KNOWLEDGE_SOURCE, AGGREGATOR_KNOWLEDGE_SOURCES
