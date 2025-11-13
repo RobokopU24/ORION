eef415ddfbfb9bf91f85b9f88157673bf9b9077f
Evan Morris <evandietzmorris@gmail.com>
2025-11-13 00:55:41 -0500

    fixing type handling for spo and source breakdown

--- Common/kgx_validation.py
+++ Common/kgx_validation.py
@@ -8,12 +8,21 @@ from Common.biolink_utils import BiolinkUtils, BiolinkInformationResources, \
 from Common.biolink_constants import *
 
 
+# this just sorts a dicts keys by its values
 def sort_dict_by_values(dict_to_sort):
     return dict(sorted(dict_to_sort.items(), key=lambda item_tuple: item_tuple[1], reverse=True))
 
 def convert_type_count_dict_to_list(type_count_dict):
     type_count_dict = sort_dict_by_values(type_count_dict)
-    return [{"type": list(types), "count": count} for types, count in type_count_dict.items()]
+    return [{"type": list(types) if types != [None] else None,
+             "count": count} for types, count in type_count_dict.items()]
+
+def convert_spo_count_to_dict(spo_count_dict):
+    return [{'subject_type': list(subject_type),
+             'predicate': predicate,
+             'object_type': list(object_type),
+             'count': count}
+            for (subject_type, predicate, object_type), count in spo_count_dict.items()]
 
 def validate_graph(nodes_file_path: str,
                    edges_file_path: str,
@@ -74,7 +83,7 @@ def validate_graph(nodes_file_path: str,
     edges_with_publications = defaultdict(int)  # predicate to num of edges with that predicate and publications
     spo_type_counts = defaultdict(int)
 
-    source_breakdown = {"aggregator": {}}
+    source_breakdown = defaultdict(lambda: defaultdict(dict))
 
     invalid_edges_due_to_predicate_and_node_types = 0
     invalid_edges_due_to_missing_primary_ks = 0
@@ -102,37 +111,30 @@ def validate_graph(nodes_file_path: str,
             if save_invalid_edges:
                 invalid_edge_output.write(f'{orjson.dumps(edge_json)}\n')
 
-        # Get aggregator knowledge sources
-        # TODO discuss whether we treat the graph as an aggregator
-        aggregator_knowledge_sources = edge_json.get(AGGREGATOR_KNOWLEDGE_SOURCES, [graph_id])
-        agg_key = frozenset(sorted(aggregator_knowledge_sources))
-        if aggregator_knowledge_sources:
-            all_aggregator_knowledge_sources.update(aggregator_knowledge_sources)
-
-        # gather metadata about knowledge sources
         all_primary_knowledge_sources.add(primary_knowledge_source)
 
-        # Initialize aggregator entry if it doesn't exist
-        if agg_key not in source_breakdown["aggregator"]:
-            source_breakdown["aggregator"][agg_key] = {
-                "primary": {}
-            }
-        agg_entry = source_breakdown["aggregator"][agg_key]
+        # Get aggregator knowledge sources
+        aggregator_knowledge_sources = edge_json.get(AGGREGATOR_KNOWLEDGE_SOURCES)
+        if aggregator_knowledge_sources is not None:
+            all_aggregator_knowledge_sources.update(aggregator_knowledge_sources)
+        else:
+            # if there isn't one use the graph id as one to organize the source breakdown
+            aggregator_knowledge_sources = [None]
 
-        # Initialize primary source entry under this aggregator if it doesn't exist
-        if primary_knowledge_source not in agg_entry["primary"]:
-            agg_entry["primary"][primary_knowledge_source] = {
-                "node_count": 0,
-                "edge_count": 0,
-                "subject_prefixes": defaultdict(int),
-                "subject_types": defaultdict(int),
-                "predicates": defaultdict(int),
-                "object_prefixes": defaultdict(int),
-                "object_types": defaultdict(int),
-                "s-p-o_types": defaultdict(int),
-                "node_set": set()
-            }
-        primary_entry = agg_entry["primary"][primary_knowledge_source]
+        agg_key = frozenset(sorted(aggregator_knowledge_sources))
+        agg_entry = source_breakdown[agg_key]
+        agg_entry[primary_knowledge_source] = {
+            "node_count": 0,
+            "edge_count": 0,
+            "subject_prefixes": defaultdict(int),
+            "subject_types": defaultdict(int),
+            "predicates": defaultdict(int),
+            "object_prefixes": defaultdict(int),
+            "object_types": defaultdict(int),
+            "s-p-o_types": defaultdict(int),
+            "node_set": set()
+        }
+        primary_entry = agg_entry[primary_knowledge_source]
 
         # Update counts within the primary entry
         primary_entry["edge_count"] += 1
@@ -171,8 +173,8 @@ def validate_graph(nodes_file_path: str,
     if save_invalid_edges:
         invalid_edge_output.close()
 
-    for agg_data in source_breakdown["aggregator"].values():
-        for prim_data in agg_data["primary"].values():
+    for agg_key, agg_data in source_breakdown.items():
+        for prim_key, prim_data in agg_data.items():
             prim_data["node_count"] = len(prim_data["node_set"])
             del prim_data["node_set"]
 
@@ -181,7 +183,10 @@ def validate_graph(nodes_file_path: str,
             prim_data["predicates"] = sort_dict_by_values(dict(prim_data["predicates"]))
             prim_data["object_prefixes"] = sort_dict_by_values(dict(prim_data["object_prefixes"]))
             prim_data["object_types"] = convert_type_count_dict_to_list(prim_data["object_types"])
-            prim_data["s-p-o_types"] = convert_type_count_dict_to_list(prim_data["s-p-o_types"])
+
+            prim_data["s-p-o_types"] = convert_spo_count_to_dict(prim_data["s-p-o_types"])
+    source_breakdown = [{"aggregator": list(agg), "breakdown": dict(breakdown)}
+                        for agg, breakdown in source_breakdown.items()]
 
     # validate the knowledge sources with the biolink model
     bl_inforesources = BiolinkInformationResources()
@@ -215,7 +220,7 @@ def validate_graph(nodes_file_path: str,
     qc_metadata['predicate_totals'] = sort_dict_by_values({k: v for k, v in predicate_counts.items()})
     qc_metadata['edges_with_publications'] = sort_dict_by_values({k: v for k, v in edges_with_publications.items()})
     qc_metadata['edge_properties'] = sorted(all_edge_properties)
-    qc_metadata['s-p-o_types'] = sort_dict_by_values({k: v for k, v in spo_type_counts.items()})
+    qc_metadata['s-p-o_types'] = convert_spo_count_to_dict(spo_type_counts)
     qc_metadata['source_breakdown'] = source_breakdown
     qc_metadata['invalid_edges_due_to_predicate_and_node_types'] = invalid_edges_due_to_predicate_and_node_types
     qc_metadata['invalid_edges_due_to_missing_primary_ks'] = invalid_edges_due_to_missing_primary_ks
