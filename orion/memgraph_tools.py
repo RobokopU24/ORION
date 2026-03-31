6420d46dd494e382f2ce06ff9fca1932546f7090
Evan Morris <evandietzmorris@gmail.com>
2026-03-31 16:31:54 -0400

    improving checks for existing memgraph dump files

--- orion/memgraph_tools.py
+++ orion/memgraph_tools.py
@@ -23,27 +23,28 @@ def create_memgraph_dump(nodes_filepath: str,
     output_csv_node_file = os.path.join(output_directory, f'memgraph_{sub_name}_nodes.csv')
     output_cypher_node_idx_file = os.path.join(output_directory,
                                                f'memgraph_{sub_name}_indexes.cypher')
-    output_edge_file = os.path.join(output_directory, f'memgraph_{sub_name}_edges.csv')
-    if (os.path.exists(output_csv_node_file) and os.path.exists(output_cypher_node_idx_file) and
-            os.path.exists(output_edge_file)):
-        logger.info(f'Memgraph files were already created for {graph_id}({graph_version})')
-    else:
-        logger.info(f'Creating memgraph dump files for {graph_id}({graph_version})...')
-        try:
-            if not os.path.exists(output_csv_node_file):
-                kgx_file_converter.convert_node_jsonl_to_memgraph_csv(
-                    nodes_input_file=nodes_filepath,
-                    output_file=output_csv_node_file,
-                    node_property_ignore_list=node_property_ignore_list)
+    output_edge_base_file = os.path.join(output_directory, f'memgraph_{sub_name}_edges.csv')
+    output_edge_manifest = os.path.splitext(output_edge_base_file)[0] + '_manifest.txt'
 
-            if not os.path.exists(output_cypher_node_idx_file):
-                kgx_file_converter.add_indexes_to_memgraph_cypher(nodes_filepath, output_cypher_node_idx_file)
+    try:
+        if not os.path.exists(output_csv_node_file):
+            logger.info(f'Creating memgraph node csv for {graph_id}({graph_version})...')
+            kgx_file_converter.convert_node_jsonl_to_memgraph_csv(
+                nodes_input_file=nodes_filepath,
+                output_file=output_csv_node_file,
+                node_property_ignore_list=node_property_ignore_list)
 
+        if not os.path.exists(output_cypher_node_idx_file):
+            logger.info(f'Creating memgraph index cypher for {graph_id}({graph_version})...')
+            kgx_file_converter.add_indexes_to_memgraph_cypher(nodes_filepath, output_cypher_node_idx_file)
+
+        if not os.path.exists(output_edge_manifest):
+            logger.info(f'Creating memgraph edge csvs for {graph_id}({graph_version})...')
             kgx_file_converter.convert_edge_jsonl_to_memgraph_csv(edges_input_file=edges_filepath,
-                                                                  output_base_file=output_edge_file,
+                                                                  output_base_file=output_edge_base_file,
                                                                   edge_property_ignore_list=edge_property_ignore_list)
-        except Exception as e:
-            logger.error(f'create_memgraph_dump() failed with exception: {e}')
-            return False
-        logger.info(f'Memgraph dump files created for {graph_id}({graph_version}).')
+    except Exception as e:
+        logger.error(f'create_memgraph_dump() failed with exception: {e}')
+        return False
+    logger.info(f'Memgraph dump files created for {graph_id}({graph_version}).')
     return True
