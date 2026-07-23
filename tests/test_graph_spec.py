09f47633c10b1767b078cfbb8ba0ef25920a8f2c
Evan Morris <evandietzmorris@gmail.com>
2026-07-22 22:48:38 -0400

    updating test

--- tests/test_graph_spec.py
+++ tests/test_graph_spec.py
@@ -304,5 +304,3 @@ def test_default_graph_spec_defines_robomouse(monkeypatch, test_graph_output_dir
         'UPhenoHumanMousePhenotypeHomology',
         'OntologicalHierarchy',
     ]
-    robomouse_sources = {source.id: source for source in robomouse_graph.sources}
-    assert robomouse_sources['UPhenoHumanMousePhenotypeHomology'].normalization_scheme.strict is False
