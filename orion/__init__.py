5289a8fe6b01dbff152f281da2e448ca1ef8ed2c
Evan Morris <evandietzmorris@gmail.com>
2026-09-23 16:05:42 -0400

    adding more constants and functions to __all__

--- orion/__init__.py
+++ orion/__init__.py
@@ -1,20 +1,26 @@
 # Public API
-from orion.kgx_file_merger import KGXFileMerger
-from orion.kgxmodel import GraphSpec, GraphSource
-from orion.kgx_metadata import KGXGraphMetadata, KGXKnowledgeSource, generate_schema
+from orion.kgx_file_merger import KGXFileMerger, MERGE_METADATA_FILENAME
+from orion.kgxmodel import GraphSpec, GraphSource, GraphFileSource
+from orion.kgx_metadata import (KGXGraphMetadata, KGXKnowledgeSource, KGXKnowledgeGraphSource, generate_schema,
+                                ORION_BUILD_VERSION, ORION_BABEL_VERSION, ORION_BIOLINK_VERSION,
+                                ORION_NODE_COUNT, ORION_EDGE_COUNT)
 from orion.kgx_schema_diff import diff_schemas
 from orion.meta_kg import MetaKnowledgeGraphBuilder
 from orion.merging import MERGING_CODE_VERSION
 from orion.kgx_file_normalizer import KGXFileNormalizer
-from orion.normalization import NodeNormalizer, NormalizationScheme, NORMALIZATION_CODE_VERSION
+from orion.normalization import (NodeNormalizer, NormalizationScheme, NORMALIZATION_CODE_VERSION,
+                                 get_current_node_norm_version, get_current_babel_version)
 
 __all__ = [
-    "KGXFileMerger",
-    "GraphSpec", "GraphSource",
-    "KGXGraphMetadata", "KGXKnowledgeSource",
+    "KGXFileMerger", "MERGE_METADATA_FILENAME",
+    "GraphSpec", "GraphSource", "GraphFileSource",
+    "KGXGraphMetadata", "KGXKnowledgeSource", "KGXKnowledgeGraphSource",
     "generate_schema", "diff_schemas",
+    "ORION_BUILD_VERSION", "ORION_BABEL_VERSION", "ORION_BIOLINK_VERSION",
+    "ORION_NODE_COUNT", "ORION_EDGE_COUNT",
     "MetaKnowledgeGraphBuilder",
     "KGXFileNormalizer",
     "NodeNormalizer", "NormalizationScheme", "NORMALIZATION_CODE_VERSION",
+    "get_current_node_norm_version", "get_current_babel_version",
     "MERGING_CODE_VERSION"
 ]
